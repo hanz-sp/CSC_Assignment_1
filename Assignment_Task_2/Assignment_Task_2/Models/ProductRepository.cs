081c8fd20f87c920a3a5bc2a72db1642ba632acd
Chris <christopher.18@ichat.sp.edu.sg>
2021-01-09 02:30:36 +0800

    rollback

---
+++ Assignment_Task_2/Assignment_Task_2/Models/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_Task_2.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> products = new List<Product>();
+        private int _nextId = 1;
+
+        public ProductRepository()
+        {
+            Add(new Product { Name = "Tomato soup", Category = "Groceries", Price = 1.39M });
+            Add(new Product { Name = "Yo-yo", Category = "Toys", Price = 3.75M });
+            Add(new Product { Name = "Hammer", Category = "Hardware", Price = 16.99M });
+        }
+
+        public Product Add(Product item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Product item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assignment_Task_2_Practical_3/Assignment_Task_2_Practical_3/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Assignment_Task_2_Practical_3.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
